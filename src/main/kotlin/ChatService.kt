5dd4c2f9ce3c059d8205871deca2416f43618760
MaximZavgorodnev <maxpek9@yandex.ru>
2021-11-29 13:45:41 +0300

    Оптимизация чат сервиса Исправлены readingMessage sendMessage

--- src/main/kotlin/ChatService.kt
+++ src/main/kotlin/ChatService.kt
@@ -9,81 +9,119 @@ object ChatService {
 
 
     //Создаем чат
-    fun createChat(titles: String, //Название чата
-                   userTwo: User,//Пользователь кому пишется сообщение
-                   userOne: User,//Пользователь который пишет сообщение
-    ) : Chat { val newChat = Chat(chatId = idChat, chatUsers = mutableListOf(),titles = titles, unreadMessages = 0)
-        chatStorage.put(newChat.chatId,newChat)
-        idChat++
-        userTwo.memoryOfMyChats[newChat.chatId] = userOne.userId
-        userOne.memoryOfMyChats[newChat.chatId] = userTwo.userId
-        return newChat
-    }
+//    fun createChat(titles: String, //Название чата
+//                   userTwo: User,//Пользователь кому пишется сообщение
+//                   userOne: User,//Пользователь который пишет сообщение
+//    ) : Chat { val newChat = Chat(chatId = idChat, chatUsers = mutableListOf(),titles = titles, unreadMessages = 0)
+//        chatStorage.put(newChat.chatId,newChat)
+//        idChat++
+//        userTwo.memoryOfMyChats[newChat.chatId] = userOne.userId
+//        userOne.memoryOfMyChats[newChat.chatId] = userTwo.userId
+//        return newChat
+//    }
 
     //Отправить сообщение пользователю(создать чат, сообщение и отправить его)
     fun sendMessage(userTwo: User,//Пользователь кому пишется сообщение
                     userOne: User,//Пользователь который пишет сообщение
                     text: String) //Текст сообщения
-                                                {
-        //Проверка на присутствие чата между пользователями
-        when{
-            !chatStorage.isEmpty() -> {
-                when (userTwo.memoryOfMyChats.containsValue(userOne.userId)) {
-                    true -> {
-                        val number = userTwo.memoryOfMyChats.filterValues { it == userOne.userId}.keys
-                        val chatId = chatStorage.keys.first { it == number.elementAt(0) }
-                        createMessage(userTwo,userOne, text, chatId)
-                    }
-                    else -> {
-                        println("Создан новый чат между пользователями ${userTwo.userName} и ${userOne.userName}")
-                        val chat = createChat(
-                            "Чат между пользователями ${userTwo.userName} и ${userOne.userName}",
-                            userTwo,//Айди второго участника чата
-                            userOne) //Айди первого участника чата))
-                        createMessage(userTwo,userOne, text, chat.chatId)
-                    }
-                }
-            }
-            else -> {
-                println("Создан новый чат между пользователями ${userTwo.userName} и ${userOne.userName}")
-                    val chat = createChat("Чат между пользователями ${userTwo.userName} и ${userOne.userName}",
-                    userTwo,//Айди кому написали
-                    userOne)//Айди кто написал
-                createMessage(userTwo,userOne, text, chat.chatId)
-            }
+    {
+       //Написание сообщения по новому
+        try {
+            val chatId = userTwo.memoryOfMyChats.filter { userTwo.memoryOfMyChats.containsKey(userOne.userId) }.getValue(userOne.userId)
+
+            var message = Message(
+                messageId = idMessage , //Идентификатор сообщения
+                chatId = chatId, //Идентификатор чата
+                messageRecipientId = userTwo.userId, //Получатель сообщения
+                messageSenderId = userOne.userId, //Отправитель сообщения
+                userNameSender = userOne.userName,//Имя отправителя сообщения
+                text = text, //Текст сообщения
+                dateTime = timeMessageCall, // Дата и время создания сообщения в формате Unixtime
+                readabilityId = false )
+            idMessage++
+            chatStorage.getValue(chatId).chatUsers.add(message)
+            chatStorage.getValue(chatId).unreadMessages++
+            println("Сообщение отправлено")
+        } catch (e: NoSuchElementException){
+//            println("Чата между ними нет")
+            println("Создан новый чат между пользователями ${userTwo.userName} и ${userOne.userName}")
+            val newChat = Chat(chatId = idChat, chatUsers = mutableListOf(),titles = "Чат между пользователями ${userTwo.userName} и ${userOne.userName}", unreadMessages = 0)
+            chatStorage.put(newChat.chatId, newChat)
+            idChat++
+            userTwo.memoryOfMyChats[userOne.userId] = newChat.chatId
+            userOne.memoryOfMyChats[userTwo.userId] = newChat.chatId
         }
+
+
+
+        //Проверка на присутствие чата между пользователями
+
+//
+//        var chatId = chatStorage.filter { chatStorage.isNullOrEmpty() }.keys.first { it == userTwo.memoryOfMyChats.filterValues { it == userOne.userId}.keys.elementAt(0) }
+//        createMessage(userTwo,userOne, text, chatId)
+//
+//        chatId = chatStorage.filter { userTwo.memoryOfMyChats.containsValue(userOne.userId) }.keys.first { it == userTwo.memoryOfMyChats.filterValues { it == userOne.userId}.keys.elementAt(0) }
+//        val chatId1 = chatStorage.filter { !userTwo.memoryOfMyChats.containsValue(userOne.userId) }.values.reduce { acc, chat -> val ch = createChat() }
+//
+//
+//        when{
+//            !chatStorage.isEmpty() -> {
+//                when (userTwo.memoryOfMyChats.containsValue(userOne.userId)) {
+//                    true -> {
+//                        val number = userTwo.memoryOfMyChats.filterValues { it == userOne.userId}.keys
+//                        val chatId = chatStorage.keys.first { it == number.elementAt(0) }
+//                        createMessage(userTwo,userOne, text, chatId)
+//                    }
+//                    else -> {
+//                        println("Создан новый чат между пользователями ${userTwo.userName} и ${userOne.userName}")
+//                        val chat = createChat(
+//                            "Чат между пользователями ${userTwo.userName} и ${userOne.userName}",
+//                            userTwo,//Айди второго участника чата
+//                            userOne) //Айди первого участника чата))
+//                        createMessage(userTwo,userOne, text, chat.chatId)
+//                    }
+//                }
+//            }
+//            else -> {
+//                println("Создан новый чат между пользователями ${userTwo.userName} и ${userOne.userName}")
+//                    val chat = createChat("Чат между пользователями ${userTwo.userName} и ${userOne.userName}",
+//                    userTwo,//Айди кому написали
+//                    userOne)//Айди кто написал
+//                createMessage(userTwo,userOne, text, chat.chatId)
+//            }
+//        }
     }
 
-    //Создание сообщения
-    fun createMessage(userTwo: User,//Пользователь кому пишется сообщение
-                      userOne: User,//Пользователь который пишет сообщение
-                      text: String, //Текст сообщения
-                      chatId: Int) {
-        var message = Message(
-            messageId = idMessage , //Идентификатор сообщения
-            chatId = chatId, //Идентификатор чата
-            messageRecipientId = userTwo.userId, //Получатель сообщения
-            messageSenderId = userOne.userId, //Отправитель сообщения
-            userNameSender = userOne.userName,//Имя отправителя сообщения
-            text = text, //Текст сообщения
-            dateTime = timeMessageCall, // Дата и время создания сообщения в формате Unixtime
-            readabilityId = false )
-        idMessage++
-        chatStorage[chatId]!!.chatUsers.add(message)
-        chatStorage[chatId]!!.unreadMessages++
-    }
+//    //Создание сообщения
+//    fun createMessage(userTwo: User,//Пользователь кому пишется сообщение
+//                      userOne: User,//Пользователь который пишет сообщение
+//                      text: String, //Текст сообщения
+//                      ) {
+//
+//
+//    }
 
     //Прочтение сообщения
     fun readingMessage(messageId: Int, chatId: Int){
-        when(chatStorage[chatId]?.chatUsers?.first{ messageId == it.messageId }?.readabilityId){
-            false -> {
-                chatStorage[chatId]!!.chatUsers.first{ messageId == it.messageId }.readabilityId = true //Прочли сообщение
-                println("Сообщение прочитано")
-                chatStorage[chatId]!!.unreadMessages--
-            }
-            true -> println("Сообщение уже было прочитано")
-            null -> println("Сообщения не существует")
+        try {
+            chatStorage.filter { !chatStorage.getValue(chatId).chatUsers.first { messageId == it.messageId }
+                .readabilityId }.getValue(chatId).chatUsers.first { messageId == it.messageId }.readabilityId = true
+            println("Cообщение прочитано")
+            val nhi  = chatStorage.getValue(chatId).chatUsers.first { messageId == it.messageId }.readabilityId
+        } catch (e: NoSuchElementException){
+            println("Cообщениz не существует")
         }
+
+
+//        when(chatStorage[chatId]?.chatUsers?.first{ messageId == it.messageId }?.readabilityId){
+//            false -> {
+//                chatStorage[chatId]!!.chatUsers.first{ messageId == it.messageId }.readabilityId = true //Прочли сообщение
+//                println("Сообщение прочитано")
+//                chatStorage[chatId]!!.unreadMessages--
+//            }
+//            true -> println("Сообщение уже было прочитано")
+//            null -> println("Сообщения не существует")
+//        }
     }
 
     //Удалить чаты пользователя
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,7 +10,7 @@ fun main() {
 //    Петя пишет ивану
     service.sendMessage(userTwo, userOne, "Привет! Как дела")
     service.sendMessage(userOne, userTwo, "Привет! Все норм")
-    service.deleteChat(userTree, userTwo)
+//    service.deleteChat(userTree, userTwo)
     service.sendMessage(userTree, userTwo,"Привет Макс!")
     service.sendMessage(userTree, userOne,"Здарова Макс!")
 
@@ -23,32 +23,32 @@ fun main() {
     service.readingMessage(2,1)
     service.readingMessage(6,1)
     service.readingMessage(3,2)
-
-//    service.readingMessage()
-
-
-
-    println(service.chatStorage.values) // вывод всех чатов в памяти
-    println()
-    println("Все чаты $userOne "+ service.outputChats(userOne)) //Вывод всех чатов Ивана
-    println()
-    println("Вывод списка сообщений чата в котором участвует Петя")
-    println(service.getListOfMessages(userTwo, 1))
 //
-    service.editMessageInChat(userTwo, "Не, все плохо, Макс", 0)
-    println("Все чаты $userTwo " + service.outputChats(userTwo)) //Вывод всех чатов Ивана
-    println(service.getListOfMessages(userTwo, 0))
-//    println("11111 чат")
-//    println(service.chatStorage[0]!!.chatUsers) // вывод всех чатов в памяти
-//    println("22222 чат")
-//    println(service.chatStorage[1]!!.chatUsers) // вывод всех чатов в памяти
-//    println("33333 чат")
-//    println(service.chatStorage[2]!!.chatUsers) // вывод всех чатов в памяти
-
-    service.deleteChat(userTree, userOne)
-    println("Количество чатов с непрочитанными сообщениями: " + service.getUnreadChatsCount())
-
-    println(service.getListOfChatsWithMessages(userTwo))
-
-    service.deleteMessage(userOne, 1)
+////    service.readingMessage()
+//
+//
+//
+//    println(service.chatStorage.values) // вывод всех чатов в памяти
+//    println()
+//    println("Все чаты $userOne "+ service.outputChats(userOne)) //Вывод всех чатов Ивана
+//    println()
+//    println("Вывод списка сообщений чата в котором участвует Петя")
+//    println(service.getListOfMessages(userTwo, 1))
+////
+//    service.editMessageInChat(userTwo, "Не, все плохо, Макс", 0)
+//    println("Все чаты $userTwo " + service.outputChats(userTwo)) //Вывод всех чатов Ивана
+//    println(service.getListOfMessages(userTwo, 0))
+////    println("11111 чат")
+////    println(service.chatStorage[0]!!.chatUsers) // вывод всех чатов в памяти
+////    println("22222 чат")
+////    println(service.chatStorage[1]!!.chatUsers) // вывод всех чатов в памяти
+////    println("33333 чат")
+////    println(service.chatStorage[2]!!.chatUsers) // вывод всех чатов в памяти
+//
+//    service.deleteChat(userTree, userOne)
+//    println("Количество чатов с непрочитанными сообщениями: " + service.getUnreadChatsCount())
+//
+//    println(service.getListOfChatsWithMessages(userTwo))
+//
+//    service.deleteMessage(userOne, 1)
 }
